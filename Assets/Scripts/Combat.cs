ab2227f7927df9f1aed2a3fd07b84511fe7fc5b8
freesdw <571683955@qq.com>
2021-06-19 18:51:19 +0800

    push file

---
+++ Assets/Scripts/Combat.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Combat : NetworkBehaviour
+{
+    public const int maxHealth = 100;
+    public AudioSource audio;
+    [SyncVar]
+    public int health = maxHealth;
+
+    public void TakeDamage(int amount)
+    {
+        if (!isServer)
+            return;
+
+        health -= amount;
+        if (health <= 0)
+        {
+            health = maxHealth;
+            // called on the server, will be invoked on the clients
+            RpcRespawn();
+        }
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+        {
+            audio.Play();
+            transform.position = new Vector3(-8, 0.5f, -4);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour
+{
+    public GameObject bulletPrefab;
+
+    private float movementInputValue;
+    private float turnInputValue;
+    private float speed = 4f;
+    private Rigidbody m_rigidbody;
+    private float turnSpeed = 150f;
+
+    private void Awake()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        // This [Command] code is run on the server!
+
+        // create the bullet object locally
+        var bullet = (GameObject)Instantiate(
+             bulletPrefab,
+             transform.position + transform.forward * 0.7f,
+             Quaternion.identity);
+
+        bullet.GetComponent<Rigidbody>().velocity = transform.forward * 5;
+
+        // spawn the bullet on the clients
+        NetworkServer.Spawn(bullet);
+
+        // when the bullet is destroyed on the server it will automaticaly be destroyed on clients
+        Destroy(bullet, 6.0f);
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        movementInputValue = Input.GetAxis("Vertical");
+        turnInputValue = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Command function is called from the client, but invoked on the server
+            CmdFire();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Turn();
+    }
+
+    private void Move()
+    {
+        Vector3 movement = transform.forward * movementInputValue * speed * Time.deltaTime;
+        m_rigidbody.MovePosition(m_rigidbody.position + movement);
+    }
+
+    private void Turn()
+    {
+        float turn = turnInputValue * turnSpeed * Time.deltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+        m_rigidbody.MoveRotation(m_rigidbody.rotation * turnRotation);
+    }
+}
